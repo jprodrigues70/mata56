00a89a21948c539a8d96101af9ee0d0808fa5efa
JP Rodrigues <joaorodrigues@infojr.com.br>
2017-01-21 22:33:27 -0300

    Adiciona verificação de choque entre turmas

--- prolog/index.pl
+++ prolog/index.pl
@@ -8,3 +8,15 @@ prereq_transitivo(X, Y) :- prereq(X, Y).
 choque_horario(horario(D, I, F), horario(H, S, E)) :-
   (D = H, I >= S, I < E);
   (D = H, S >= I, S < F).
+
+%%% Verificação de choque entre aulas %%%
+choque_aulas([H|_], [A|_]) :- choque_horario(H, A).
+choque_aulas(A, B) :- verify(A, B); verify(B, A).
+
+verify([H|T], L) :- member(H, L); choque_aulas(T, L).
+
+%%% Verificação de choque entre turmas %%%
+choque_turma(turma(X, Y), turma(X, Y)).
+choque_turma(turma(X, Y), turma(A, B)) :-
+  aulas(turma(X, Y), L), aulas(turma(A, B), M),
+  choque_aulas(L, M).
