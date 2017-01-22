1ca257b3f34910cec058bc0cd2e81a6adfb9b83d
JP Rodrigues <joaorodrigues@infojr.com.br>
2017-01-21 22:18:46 -0300

    Remove verificação incorreta de vazio

--- prolog/index.pl
+++ prolog/index.pl
@@ -1,6 +1,5 @@
 %%% Verificação de Pré-requisitos %%%
 
-prereq_transitivo([], []) :- [].
 prereq_transitivo(X, Y) :- prereq(X, Z),
 prereq_transitivo(Z, Y).
 prereq_transitivo(X, Y) :- prereq(X, Y).
