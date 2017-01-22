eb3fdc7c46376fc3988cb06fc370fe36e6e91c98
JP Rodrigues <joaorodrigues@infojr.com.br>
2017-01-21 22:21:31 -0300

    Adiciona verificação de choque de horário

--- prolog/index.pl
+++ prolog/index.pl
@@ -3,3 +3,8 @@
 prereq_transitivo(X, Y) :- prereq(X, Z),
 prereq_transitivo(Z, Y).
 prereq_transitivo(X, Y) :- prereq(X, Y).
+
+%%% Verificação de choque de horário %%%
+choque_horario(horario(D, I, F), horario(H, S, E)) :-
+  (D = H, I >= S, I < E);
+  (D = H, S >= I, S < F).
